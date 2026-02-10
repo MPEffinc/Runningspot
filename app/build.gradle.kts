2f2ff4cf0535e8896ff7c03025c7c86bf643ee6e
nambao03 <153759802a@inu.ac.kr>
2026-02-10 17:00:12 +0900

    git commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,4 +102,6 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
