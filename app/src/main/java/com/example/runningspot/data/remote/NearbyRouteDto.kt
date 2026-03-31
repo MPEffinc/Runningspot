00238253f84ef650b6df0fbfa3aba392d2a7490e
nambao03 <153759802a@inu.ac.kr>
2026-03-31 19:32:22 +0900

    commit

--- app/src/main/java/com/example/runningspot/data/remote/NearbyRouteDto.kt
+++ app/src/main/java/com/example/runningspot/data/remote/NearbyRouteDto.kt
@@ -6,5 +6,6 @@ data class NearbyRouteDto(
     val distance_m: Double, // 루트 총 거리(미터)
     val start_lat: Double,  // 시작 지점 위도
     val start_lng: Double,   // 시작 지점 경도
-    val profile_image_url: String?
+    val profile_image_url: String?,
+    val nickname: String? = null
 )
